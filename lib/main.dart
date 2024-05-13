e14c4aef3b1df4b3a90d4bfbbfc31c296933adf2
Jamoliddin developer <jamoliddinvahobov76@gmail.com>
2024-05-12 20:16:15 -0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+// ignore_for_file: avoid_print
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:my_notes/firebase_options.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'My Notes',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: const HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        backgroundColor: Colors.green,
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              if (user?.emailVerified ?? false) {
+                print("You are a verified user");
+              } else {
+                print("You need to verify your email first");
+              }
+              return const Text(
+                "it has been done why do you need this git is a allows you to manage various changes the person who made,come on dawg",
+                style: TextStyle(color: Colors.green, fontSize: 40),
+              );
+
+            default:
+              return const Text("Loading...");
+          }
+        },
+      ),
+    );
+  }
+}
+
+
+// staypeachy707@gmail.com
+
+// abc1234',
